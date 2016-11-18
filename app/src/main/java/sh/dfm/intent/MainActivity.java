d9d4ed4ef1660b43951ebcbdf367d74ba2e3244f
Beletskiy Dmitrii <legioner009@gmail.com>
2016-11-19 00:57:00 +0200

    comment Req

--- app/src/main/java/sh/dfm/intent/MainActivity.java
+++ app/src/main/java/sh/dfm/intent/MainActivity.java
@@ -1,17 +1,24 @@
 package sh.dfm.intent;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvRData;
     Button btnSend;
+    Button btnSendError;
+
+    public static MainActivity link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,28 +27,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tvRData = (TextView) findViewById(R.id.tvRData);
         btnSend = (Button) findViewById(R.id.btn1);
+        btnSendError = (Button) findViewById(R.id.btnError);
 
         btnSend.setOnClickListener(this);
+        btnSendError.setOnClickListener(this);
+        link = this;
     }
 
 
-     @Override
+    @Override
     public void onClick(View v) {
-        EditText messageView = (EditText) findViewById(R.id.ed1);
-        String message = messageView.getText().toString();
+        if (v.getId() == R.id.btn1) {
+            EditText messageView = (EditText) findViewById(R.id.ed1);
+            String message = messageView.getText().toString();
 
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(SecondActivity.EXTRA_MESSAGE, message);
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra(SecondActivity.EXTRA_MESSAGE, message);
             startActivityForResult(intent, 1);
         }
+        if (v.getId() == R.id.btnError) {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("text/error");
+            //startActivity(openlinkIntent);
+
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, 1);
+
+            }else{
+              Toast.makeText(getApplicationContext() ,
+                        getString(R.string.app_name).toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-       // super.onActivityResult(requestCode, resultCode, data);
-        if(data == null){return;}
+        // super.onActivityResult(requestCode, resultCode, data);
+        if (data == null) {
+            return;
+        }
         String rData = data.getStringExtra("rData");
         tvRData.setText(rData);
     }
