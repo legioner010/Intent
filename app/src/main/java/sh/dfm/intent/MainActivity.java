9cab1905c31501eb8f40bc0bef02b2499d3f988a
Beletskiy Dmitrii <legioner009@gmail.com>
2016-11-18 23:52:48 +0200

    resOK

--- app/src/main/java/sh/dfm/intent/MainActivity.java
+++ app/src/main/java/sh/dfm/intent/MainActivity.java
@@ -4,37 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView tvRData;
+    Button btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvRData = (TextView) findViewById(R.id.tvRData);
+        btnSend = (Button) findViewById(R.id.btn1);
+
+        btnSend.setOnClickListener(this);
     }
 
 
-    public void btn1Click(View view) {
+     @Override
+    public void onClick(View v) {
         EditText messageView = (EditText) findViewById(R.id.ed1);
-        String message= messageView.getText().toString();
+        String message = messageView.getText().toString();
 
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(SecondActivity.EXTRA_MESSAGE,message);
+        intent.putExtra(SecondActivity.EXTRA_MESSAGE, message);
 
-
-//   Verufy that intent will resilve to an activity
-        if(intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
         }
+    }
 
-
-
-//        Intent intent = new Intent(Intent.ACTION_PICK);
-//        intent.setType("*/*");
-
-      //  startActivity(intent);
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       // super.onActivityResult(requestCode, resultCode, data);
+        if(data == null){return;}
+        String rData = data.getStringExtra("rData");
+        tvRData.setText(rData);
     }
 }
--- app/src/main/java/sh/dfm/intent/SecondActivity.java
+++ app/src/main/java/sh/dfm/intent/SecondActivity.java
@@ -4,27 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String EXTRA_MESSAGE ="id";
+    public static final String EXTRA_MESSAGE = "id";
+
+    EditText etRDara;
+    Button btnOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        etRDara = (EditText) findViewById(R.id.edRData);
+        btnOK = (Button) findViewById(R.id.btnSendRData);
+        btnOK.setOnClickListener(this);
+
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(EXTRA_MESSAGE);
-
         TextView tv2 = (TextView) findViewById(R.id.tv2);
         tv2.setText(message);
     }
 
+
     @Override
-    protected void onDestroy() {
-        Log.d("SecondActivity", "activity destroy");
-        super.onDestroy();
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.putExtra("rData", etRDara.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
